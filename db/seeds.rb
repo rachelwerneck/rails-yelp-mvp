4060420d7966d6a785cdba3a13cf72e2085846fd
Rachel Werneck <rachelwerneck@gmail.com>
2022-08-17 16:24:05 -0300

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants..."
+fast_food = {name: 'McDonald', address: 'Flamengo', phone_numer: '552122252693', category: 'french'}
+pizza =  {name: 'Vespa', address: 'botafogo', phone_numer: '552122252694', category: 'italian'}
+burguer = {name: 'TTBurger', address: 'Ipanema', phone_numer: '552122252695', category: 'belgian'}
+ice_cream = {name: 'Gelato', address: 'Leblon', phone_numer: '552122252696', category: 'chinese'}
+desert = {name: 'Cookie', address: 'Gavea', phone_numer: '552122252697', category: 'japonese'}
+
+[fast_food, pizza, burguer, ice_cream, desert].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
